8c33182f37c64694f3ea3d6da02714e224018b5e
aya <ayapapaya.uu@gmail.com>
2020-04-30 15:55:08 -0700

    fix typo

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import reducer from './reducers'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
   //<React.StrictMode>
   <Provider store={store}>  
 	<App />
-  </Provider>
+  </Provider>,
   //</React.StrictMode>,
   document.getElementById('root')
 );
