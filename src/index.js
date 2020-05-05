8dd0c1ff48ad6c4c63a37bc55138619abf36d3e2
aya <ayapapaya.uu@gmail.com>
2020-05-05 11:00:43 -0700

    implement event new component

--- src/index.js
+++ src/index.js
@@ -3,19 +3,24 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new'
 import * as serviceWorker from './serviceWorker';
-//import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <Provider store={store}>  
-	<EventsIndex />
+	  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+		<Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
@@ -24,4 +29,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-//registerServiceWorker();
